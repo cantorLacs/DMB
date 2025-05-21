5c319a62c1c25c405cc9b95b86f2ede0489399f3
LuisCantor <cantorlacs@gmail.com>
2025-05-21 02:23:43 -0400

    improved login/register design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lacs.testdmb"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -38,6 +38,7 @@ android {
     }
 }
 
+
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
 
@@ -58,6 +59,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
 
+
 apply(plugin = "com.google.gms.google-services")
+
